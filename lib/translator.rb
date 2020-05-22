abb2fe5af09da882858da93ef73aab72acf93b4e
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-22 21:27:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,22 +2,54 @@
 
 
 
+require "yaml"
+require 'pry'
 
-def load_library(file_path)
-  file = File.read(file_path)
-  emoticons = YAML.load(file)
+# def load_library(path)
+#   emoticons = YAML.load_file(path)
+#   emoticon_hash = {}
+#   emoticon_hash["get_emoticon"] = {}
+#   emoticon_hash["get_meaning"] = {}
+#   for emoticon in emoticons.keys
+#     emoticon_hash[emoticon] = {:english => emoticons[emoticon][0], :japanese => emoticons[emoticon][1]}
+#   end
+#   emoticon_hash
+# end  
+
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticon_hash = {}
+
+  emoticon_hash["get_emoticon"] = {}
+  emoticon_hash["get_meaning"] = {}
+
+  # emoticons.each do |english_word, emoticon_set|
+  #   emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+  #   emoticon_hash["get_meaning"][emoticon_set.last] = english_word
+  for emoticon in emoticons.keys
+    emoticon_hash[emoticon] = {:english => emoticons[emoticon][0], :japanese => emoticons[emoticon][1]}
+  end
+  
+  emoticon_hash
 end
 
-def get_japanese_emoticon(file_path, emoticon)
-  emoticons = load_library(file_path)
-  emoji = emoticons.values.find {|faces| faces[0] == emoticon}
-  emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
+
+def get_japanese_emoticon(path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_emoticon"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
 end
 
-def get_english_meaning(file_path, emoticon)
-  emoticons = load_library(file_path)
-  emoji = emoticons.find {|name, faces| faces[1] == emoticon }
-  emoji == nil ? "Sorry, that emoticon was not found" : emoji[0]
+def get_english_meaning(path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_meaning"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
 end
 
 
