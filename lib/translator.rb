c18bfa6f0a9dc88701c06060cd865465aca03a9f
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-22 21:09:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,10 +23,12 @@ def load_library(path)
   emoticon_hash["get_emoticon"] = {}
   emoticon_hash["get_meaning"] = {}
 
-  emoticons.each do |english_word, emoticon_set|
-    emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
-    emoticon_hash["get_meaning"][emoticon_set.last] = english_word
-    
+  # emoticons.each do |english_word, emoticon_set|
+  #   emoticon_hash["get_emoticon"][emoticon_set.first] = emoticon_set.last
+  #   emoticon_hash["get_meaning"][emoticon_set.last] = english_word
+  for emoticon in emoticons.keys
+     emoticon_hash[emoticon] = {:english => emoticons[emoticon][0], :japanese => emoticons[emoticon][1]}
+  end   
     
   end
   emoticon_hash
