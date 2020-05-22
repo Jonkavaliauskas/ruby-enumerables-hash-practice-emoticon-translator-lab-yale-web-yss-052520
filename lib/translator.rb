121dc7a22a71610068de650f4ba2a2e5f8578b1f
Jonas Kavaliauskas <jonas.kavaliauskas@yale.edu>
2020-05-22 21:02:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,6 +31,23 @@ def load_library(path)
 end
 
 
+def get_japanese_emoticon(path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_emoticon"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
+end
 
+def get_english_meaning(path, emoticon)
+  emoticon_hash = load_library(path)
+  result = emoticon_hash["get_meaning"][emoticon]
+  if result == nil
+    result = "Sorry, that emoticon was not found" 
+  end
+  result
+end
+© 2020 GitHub, Inc.
 
 
